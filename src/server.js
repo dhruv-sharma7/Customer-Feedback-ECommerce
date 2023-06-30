bbee0e73aced41c0863f5e05d8ab9bf46da36e11
dhruv-sharma7 <dhruv.722202@gmail.com>
2023-06-30 19:04:53 +0530

    API s

--- src/server.js
+++ src/server.js
@@ -14,3 +14,17 @@ app.get('/', (req, res) => {
   });
 
   app.use(express.static('public'));
+  app.use(express.json());
+  app.post('/api/reviews', (req, res) => {
+    const review = req.body; // Assuming the review data is sent in the request body
+  
+    // Perform server-side validation on the review data
+    if (!review.title || !review.content) {
+      res.status(400).json({ error: 'Title and content are required fields.' });
+      return;
+    }
+  
+    // Store the review in the database or perform any other required operations
+  
+    res.status(201).json({ message: 'Review submitted successfully!' });
+  });
